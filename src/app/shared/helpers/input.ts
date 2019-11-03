a6c3119d684b98ff76cc4701fa9a3ce49a74c398
Kirill Goncharov <kdgoncharov@gmail.com>
2019-11-03 18:29:43 +0300

    Web: Autofocus on task text field in form

--- src/app/shared/helpers/input.ts
+++ src/app/shared/helpers/input.ts
@@ -1,9 +1,8 @@
 import { ElementRef } from '@angular/core';
 
 export function focusOnInput(input: ElementRef) {
-    console.log(input);
+    input.nativeElement.focus();
 }
 
-export function enableInputSuggestions(input: ElementRef) {
-    console.log(input);
+export function enableInputSuggestions(input: ElementRef) { // eslint-disable-line @typescript-eslint/no-unused-vars
 }
