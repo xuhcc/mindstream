397b99970d25b7cb1123b9f8535104102493ced4
Kirill Goncharov <kdgoncharov@gmail.com>
2019-12-02 16:24:52 +0300

    Web: Fix datepicker toggle

--- src/app/shared/helpers/date-picker.ts
+++ src/app/shared/helpers/date-picker.ts
@@ -2,9 +2,12 @@ import { IMyDateModel } from 'angular-mydatepicker';
 import { first } from 'rxjs/operators';
 
 export function openDatePicker(datepicker): Promise<string> {
-    // TODO: use toggleCalendar
-    datepicker.openCalendar();
-    return new Promise<string>((resolve) => {
+    return new Promise<string>((resolve, reject) => {
+        const opened = datepicker.toggleCalendar();
+        if (!opened) {
+            reject(new Error('Picker cancelled'));
+            return;
+        }
         datepicker.dateChanged.pipe(first()).subscribe((result: IMyDateModel) => {
             resolve(result.singleDate.formatted);
         });
