35ef1e6af3406a2c778b0dfe55e14ceaa3a88d98
Kirill Goncharov <kdgoncharov@gmail.com>
2019-12-18 21:21:09 +0300

    Common: Improve custom html view
    - Prepare for for general use.
    - Don't override bold and italic elements on IOS.

--- src/app/shared/custom-html-view/custom-html-view.android.ts
+++ src/app/shared/custom-html-view/custom-html-view.android.ts
@@ -13,24 +13,48 @@ export class CustomHtmlView extends HtmlView {
 
     public initNativeView(): void {
         super.initNativeView();
-        // For consistency with iOS
+        // Allow text selection
         this.nativeViewProtected.setTextIsSelectable(true);
+        // Make links clickable
         this.nativeViewProtected.setMovementMethod(android.text.method.LinkMovementMethod.getInstance());
     }
 
-    [colorProperty.setNative](value: Color) {
-        this.nativeViewProtected.setTextColor(value.android);
+    [colorProperty.getDefault](): android.content.res.ColorStateList {
+        return this.nativeViewProtected.getTextColors();
     }
 
-    [linkColorProperty.setNative](value: Color) {
-        this.nativeViewProtected.setLinkTextColor(value.android);
+    [colorProperty.setNative](value: Color | android.content.res.ColorStateList) {
+        const color = value instanceof Color ? value.android : value;
+        this.nativeViewProtected.setTextColor(color);
     }
 
-    [fontInternalProperty.setNative](value: Font) {
-        this.nativeViewProtected.setTypeface(value.getAndroidTypeface());
+    [linkColorProperty.getDefault](): android.content.res.ColorStateList {
+        return this.nativeViewProtected.getLinkTextColors();
     }
 
-    [fontSizeProperty.setNative](value: number) {
-        this.nativeViewProtected.setTextSize(value);
+    [linkColorProperty.setNative](value: Color | android.content.res.ColorStateList) {
+        const color = value instanceof Color ? value.android : value;
+        this.nativeViewProtected.setLinkTextColor(color);
+    }
+
+    [fontInternalProperty.getDefault](): android.graphics.Typeface {
+        return this.nativeViewProtected.getTypeface();
+    }
+
+    [fontInternalProperty.setNative](value: Font | android.graphics.Typeface) {
+        const font = value instanceof Font ? value.getAndroidTypeface() : value;
+        this.nativeViewProtected.setTypeface(font);
+    }
+
+    [fontSizeProperty.getDefault](): {nativeSize: number} {
+        return {nativeSize: this.nativeViewProtected.getTextSize()};
+    }
+
+    [fontSizeProperty.setNative](value: number | {nativeSize: number}) {
+        if (typeof value === 'number') {
+            this.nativeViewProtected.setTextSize(value);
+        } else {
+            this.nativeViewProtected.setTextSize(android.util.TypedValue.COMPLEX_UNIT_PX, value.nativeSize);
+        }
     }
 }
--- src/app/shared/custom-html-view/custom-html-view.ios.ts
+++ src/app/shared/custom-html-view/custom-html-view.ios.ts
@@ -8,7 +8,20 @@ import {
 
 import { linkColorProperty } from './custom-html-view-common';
 
+function uiColorToHex(uiColor: UIColor): string {
+    const rgba = CGColorGetComponents(uiColor.CGColor); // eslint-disable-line no-undef
+    const nsColor = new Color(
+        Math.round(rgba[3] * 255),
+        Math.round(rgba[0] * 255),
+        Math.round(rgba[1] * 255),
+        Math.round(rgba[2] * 255),
+    );
+    return nsColor.hex;
+}
+
 export class CustomHtmlView extends HtmlView {
+
+    private currentHtml: string;
 
     public initNativeView(): void {
         super.initNativeView();
@@ -19,30 +32,57 @@ export class CustomHtmlView extends HtmlView {
         this.nativeViewProtected.textContainerInset = (UIEdgeInsets as any).zero; // eslint-disable-line no-undef
     }
 
-    [colorProperty.setNative](value: Color) {
-        this.nativeViewProtected.textColor = value.ios;
+    private withStyles() {
+        let html = this.currentHtml;
+        const styles = [];
+        if (this.nativeViewProtected.font) {
+            styles.push(`font-family: '${this.nativeViewProtected.font.fontName}';`);
+            styles.push(`font-size: ${this.nativeViewProtected.font.pointSize}px;`);
+        }
+        if (this.nativeViewProtected.textColor) {
+            styles.push(`color: ${uiColorToHex(this.nativeViewProtected.textColor)};`);
+        }
+        if (styles.length > 0) {
+            html += `<style>body {${styles.join('')}}</style>`;
+        }
+        super[htmlProperty.setNative](html);
     }
 
-    [linkColorProperty.setNative](value: Color) {
+    [htmlProperty.setNative](value: string) {
+        this.currentHtml = value;
+        this.withStyles();
+    }
+
+    [colorProperty.getDefault](): UIColor {
+        return this.nativeViewProtected.textColor;
+    }
+
+    [colorProperty.setNative](value: Color | UIColor) {
+        const color = value instanceof Color ? value.ios : value;
+        this.nativeViewProtected.textColor = color;
+        this.withStyles();
+    }
+
+    [linkColorProperty.getDefault](): UIColor {
+        return this.nativeViewProtected.linkTextAttributes[NSForegroundColorAttributeName]; // eslint-disable-line no-undef
+    }
+
+    [linkColorProperty.setNative](value: Color | UIColor) {
+        const color = value instanceof Color ? value.ios : value;
         const linkTextAttributes = NSDictionary.dictionaryWithObjectForKey( // eslint-disable-line no-undef
-            value.ios,
+            color,
             NSForegroundColorAttributeName, // eslint-disable-line no-undef
         );
         this.nativeViewProtected.linkTextAttributes = linkTextAttributes;
     }
 
-    [fontInternalProperty.setNative](value: Font) {
-        const font = value.getUIFont(this.nativeViewProtected.font);
-        this.nativeViewProtected.font = font;
+    [fontInternalProperty.getDefault](): UIFont {
+        return this.nativeViewProtected.font;
     }
 
-    [htmlProperty.setNative](value: string) {
-        // Reassign font after changing content
-        // https://stackoverflow.com/questions/19049917/uitextview-style-is-being-reset-after-setting-text-property
-        const font = this.nativeViewProtected.font;
-        const textColor = this.nativeViewProtected.textColor;
-        super[htmlProperty.setNative](value);
+    [fontInternalProperty.setNative](value: Font | UIFont) {
+        const font = value instanceof Font ? value.getUIFont(this.nativeViewProtected.font) : value;
         this.nativeViewProtected.font = font;
-        this.nativeViewProtected.textColor = textColor;
+        this.withStyles();
     }
 }
