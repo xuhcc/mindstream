6f16cc5913bb83fd69d8df2de553780de3bdcf29
Kirill Goncharov <kdgoncharov@gmail.com>
2019-12-31 17:21:02 +0300

    Version 1.1.0

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     backendUrl: '',
-    version: '1.0.0',
+    version: '1.1.0',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ declare const PORT;
 export const environment = {
     production: false,
     backendUrl: `http://localhost:${PORT}`,
-    version: '1.0.0-dev',
+    version: '1.1.0-dev',
 };
