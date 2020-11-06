d595be4676a4621953238c544f2d6d25ed1c252e
Kirill Goncharov <kdgoncharov@gmail.com>
2020-11-06 17:01:56 +0300

    Common: Upgrade jsTodoTxt to git master

--- webpack-tns.config.js
+++ webpack-tns.config.js
@@ -3,11 +3,5 @@ const baseConfig = require('./webpack.config');
 
 module.exports = env => {
     const config = baseConfig(env);
-    config.plugins.push(
-        // Provide global variable for jstodotxt
-        new webpack.ProvidePlugin({
-            TodoTxtExtension: ['jstodotxt/jsTodoExtensions', 'TodoTxtExtension'],
-        }),
-    );
     return config;
 };
--- webpack-web.config.js
+++ webpack-web.config.js
@@ -2,10 +2,6 @@ const webpack = require('webpack');
 
 module.exports = {
     plugins: [
-        // Provide global variable for jstodotxt
-        new webpack.ProvidePlugin({
-            TodoTxtExtension: ['jstodotxt/jsTodoExtensions', 'TodoTxtExtension'],
-        }),
         // Allow to specify app port
         new webpack.DefinePlugin({
             PORT: process.env.PORT || 8080,
